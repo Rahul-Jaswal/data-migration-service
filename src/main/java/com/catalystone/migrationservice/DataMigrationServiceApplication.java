8384cc82f61ce7b622236c0729e4738cade52f7a
Rahul Jaswal <rahul.jaswal@catalystone.com>
2020-09-08 19:14:08 +0530

    First commit

---
+++ src/main/java/com/catalystone/migrationservice/DataMigrationServiceApplication.java
@@ -0,0 +1,13 @@
+package com.catalystone.migrationservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataMigrationServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataMigrationServiceApplication.class, args);
+	}
+
+}
